647a99b56842635aee297bd0f3953f6731722602
mouad <hamideddinemouad@gmail.com>
2024-09-24 12:29:40 +0100

    Variables

---
+++ Day01/Variables/variables2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - entrée du program
+* 
+* Description: demande temperature en c et convertis au kelvin
+*
+* Return: Toujours 0 (succès)
+*/
+
+int main(void)
+{
+    float temperature;
+
+    printf("Temperature en Celsuis ?\n");
+    scanf("%f", &temperature);
+    temperature = temperature + 273.15;
+    printf("%f", temperature);
+}
