ad94c6ce6e9077271a5c5df00fbba8cbfaa051c8
mouad <hamideddinemouad@gmail.com>
2024-09-28 12:45:25 +0100

    Finish

--- Mini-projet-Système-de-Gestion-de-Stock-dans-une-Librairie/main.c
+++ Mini-projet-Système-de-Gestion-de-Stock-dans-une-Librairie/main.c
@@ -14,14 +14,20 @@ int main()
     char base_titre[100][100];
     char base_auteur[100][100];
     //char titre_entre[];
+    char base_prix_s[100];
+    char base_quantite_s[100];
+    char choix_s[100];
     int base_prix[100] = {0};
     int base_quantite[100] = {0};
     char case4_titre[100];
     char case3_titre[100];
+    int is_num = 0;
+    int checker;
     int case3_index;
     int case4_index;
     int case5_index;
     int compteur1;
+    int nettoyage;
     int choix, position_1 = 0;
     int nouvelle_quantite;
     char case5_titre[100];
@@ -34,19 +40,81 @@ int main()
     printf("4.Mettre a jour la quantite d'un livre.\n");
     printf("5.Supprimer un livre du stock.\n");
     printf("6.Afficher le nombre total de livres en stock.\n\n");
-    printf("faites votre choix:");
-    scanf("%d",&choix);
+    printf("faites votre choix:\n");
+    //choix_s[0] = '\0';
+    // NETTOYER TABLEAU
+    for (nettoyage = 0; nettoyage < strlen(choix_s); nettoyage++)
+    {
+        choix_s[nettoyage] = '/0';
+    }
+    scanf("%s", choix_s);
+    for (checker = 0; checker < strlen(choix_s); checker++)
+    {
+        if (!isdigit(choix_s[checker]))
+        {
+            printf("Que des numero !!\n");
+            choix = 999;
+            is_num = -1;
+            break;
+        }
+        is_num = 0;
+    }
+    if (is_num == 0)
+    {
+        choix = atoi(choix_s);
+        //printf("atoi(choix_s) = %d", atoi(choix_s));
+    }
+    //printf("choix = %d", choix);
     switch(choix)
     {
         case 1:
             printf("veuillez entre le titre du livre:\n");
-            scanf("%s",base_titre[position_1]);
+            scanf(" %[^\n]",base_titre[position_1]);
             printf("veuillez entre l'auteur:\n");
-            scanf("%s", base_auteur[position_1]);
+            scanf(" %[^\n]", base_auteur[position_1]);
             printf("veuillez entrer le prix du livre:\n");
-            scanf("%d", &base_prix[position_1]);
+            for (nettoyage = 0; nettoyage < strlen(base_prix_s); nettoyage++)
+            {
+                base_prix_s[nettoyage] = '/0';
+            }
+            scanf("%s", base_prix_s);
+            for (checker = 0; checker < strlen(base_prix_s); checker++)
+            {
+                if (!isdigit(base_prix_s[checker]))
+                {
+                    printf("Que des numero !!\n");
+                    is_num = -1;
+                    break;
+                }
+                is_num = 0;
+            }
+            if (is_num == 0)
+            {
+                base_prix[position_1] = atoi(base_prix_s);
+                //printf("atoi(choix_s) = %d", atoi(choix_s));
+            }
+            //scanf("%d", &base_prix[position_1]);
             printf("veuillez entrer la quantite du stock:\n");
-            scanf("%d", &base_quantite[position_1]);
+            for (nettoyage = 0; nettoyage < strlen(base_quantite_s); nettoyage++)
+            {
+                base_quantite_s[nettoyage] = '/0';
+            }
+            scanf("%s", base_quantite_s);
+            for (checker = 0; checker < strlen(base_quantite_s); checker++)
+            {
+                if (!isdigit(base_quantite_s[checker]))
+                {
+                    printf("Que des numero !!\n");
+                    is_num = -1;
+                    break;
+                }
+                is_num = 0;
+            }
+            if (is_num == 0)
+            {
+                base_quantite[position_1] = atoi(base_quantite_s);
+                //printf("atoi(choix_s) = %d", atoi(choix_s));
+            }
             //printf("%s \n", base_titre[position_1]);
             //printf("%s \n", base_auteur[position_1]);
             //printf("%d \n", base_prix[position_1]);
@@ -66,7 +134,7 @@ int main()
             break;
         case 3:
             printf("entrez le nom de livre que vous souhaitez rechercher\n");
-            scanf("%s", case3_titre);
+            scanf(" %[^\n]", case3_titre);
             for (compteur1 = 0; compteur1 != position_1; compteur1++)
             {
                 if (strcmp(case3_titre, base_titre[compteur1]) == 0)
@@ -83,7 +151,7 @@ int main()
         case 4:
             //update quantité
             printf("entrez le nom du livre que vous souhaitez changer de quantite\n");
-            scanf("%s", case4_titre);
+            scanf(" %[^\n]", case4_titre);
             for (compteur1 = 0; compteur1 != position_1; compteur1++)
             {
                 if (strcmp(case4_titre, base_titre[compteur1]) == 0)
@@ -99,20 +167,35 @@ int main()
         case 5:
             //supprimer un livre
             printf("entrez le nom du livre que vous souhaitez supprimer\n");
-            scanf("%s", case5_titre);
+            scanf(" %[^\n]", case5_titre);
+            //trouver titre
             for (compteur1 = 0; compteur1 != position_1; compteur1++)
             {
                 if (strcmp(case5_titre, base_titre[compteur1]) == 0)
                 {
                     case5_index = compteur1;
                     break;
-                    printf("fonctionalité non disponible\n");
+                    //printf("fonctionalité non disponible\n");
+                }
+                case5_index = -1;
+            }
+            //printf("case5_index%d\n", case5_index);
+            if (case5_index > 0)
+            {
+                for (compteur1 = case5_index; compteur1 <= position_1; compteur1++)
+                {
+                    printf("suppression\n");
+                    strcpy(base_titre[compteur1], base_titre[compteur1 + 1]);
                 }
             }
+            else if (case5_index < 0)
+            {
+                printf("titre introuvable\n");
+            }
         case 6:
-            printf("nombre total des livres:%d", position_1);
+            printf("nombre total des livres:%d\n", position_1);
             break;
-
+        
     }
     }
 }
